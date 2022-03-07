aa648fdfee85864ec03653a3e4e2880d8bc1cd84
Marisa Agarwal <marisaagarwal@gmail.com>
2022-03-07 13:17:06 +1000

    load in preliminary/inncomplete data

---
+++ creation_code/create_AIdata.R
@@ -0,0 +1,49 @@
+# 2022-03-07
+
+
+## 1. Set up
+
+    # point to data locale
+    data_locale = "data/"
+    
+    # point to data file
+    data_file = "AI Threshold Experiment - all categories.xlsx"
+    
+    # call to data
+    experiment_data = 
+        paste0(data_locale, data_file) %>%
+        read_excel()
+
+    # check structure of data
+    str(experiment_data)
+
+
+## 2. Groom data
+
+    # # remove extra columns
+    # experiment_data %<>%
+    #     dplyr::select(-c(11:12,))
+    
+    # make confidence threshold a factor
+      experiment_data$confidence_threshold =
+        factor(experiment_data$confidence_threshold,
+               levels = c(50,75, 100))
+    
+      str(experiment_data)
+    
+      
+## 3. Add columns
+    
+    # proportion of AI annotations completed, then prop that were correct & wrong
+    experiment_data %<>%
+        mutate(propAIannotations_confirmed = n_preconfirmed_annotations/n_annotations,
+               propAIannotations_wrong = n_preconfirmed_wrong / n_preconfirmed_annotations,
+               propAIannotations_correct = 1 - propAIannotations_wrong)
+
+    
+    
+    
+    
+    
+    
+
