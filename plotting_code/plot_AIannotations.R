4453e5a2c32def0ae1ebaae08da9274aa534ffcd
Marisa Agarwal <marisaagarwal@gmail.com>
2022-03-14 16:03:22 +1000

    updating plotting code

---
+++ plotting_code/plot_AIannotations.R
@@ -0,0 +1,13 @@
+# 2022-03-14
+
+
+## 1. Set up
+
+    # point to data locale
+    data_locale = "analysis_code/"
+    
+    # load in the data
+    source(paste0(data_locale, "analyze_AIannotations.R"))
+    
+
+## 2. 
--- plotting_code/plot_AIdata.R
+++ plotting_code/plot_AIdata.R
@@ -9,21 +9,47 @@
     source(paste0(data_locale, "analyze_AIdata.R"))
     
     str(experiment_data)
+    
+  
+## 2. Plotting number of annotations done by AI at different thresholds
 
-
-## 2. Plotting time spent per photo at different AI thresholds
+    # boxplot
+    experiment_data %>%
+      ggplot(aes(confidence_threshold, n_preconfirmed_annotations)) +
+        geom_boxplot(aes(fill = confidence_threshold), show.legend = F) +
+        scale_fill_viridis_d(alpha = 0.4) +
+        geom_signif(test = "wilcox.test", 
+                    comparisons = combn(
+                      levels(experiment_data$confidence_threshold),
+                      2,
+                      simplify = F),
+                    step_increase = 0.2,
+                    map_signif_level = T) +
+        # stat_compare_means(method = "kruskal.test", label.y = 170) +
+        labs(x = "Confidence Threshold",
+             y = "Number of Annotations Done by Feature Extractor") +
+        theme_pubr()
+    
+    
+## 3. Plotting time spent per photo at different thresholds
     
     # boxplot
     experiment_data %>%
         ggplot(aes(confidence_threshold, time_spent)) +
             geom_boxplot(aes(fill = confidence_threshold), show.legend = F) +
             scale_fill_viridis_d(alpha = 0.4) +
-            geom_signif(comparisons = list(c("50", "75")), 
-                        map_signif_level = TRUE,
-                        y_position = 60) +
-            labs(x = "AI Confidence Threshold",
-                 y = "Time Spent (sec/photo)") +
-            theme_light()
+            geom_signif(test = "wilcox.test", 
+                        comparisons = combn(
+                            levels(experiment_data$confidence_threshold),
+                            2,
+                            simplify = F),
+                        step_increase = 0.2,
+                        map_signif_level = T) +
+            # stat_compare_means(method = "kruskal.test", label.y = 170) +
+            labs(x = "Confidence Threshold",
+                 y = "Annotation Time (sec/photo)") +
+            theme_pubr()
+    
     
     # pointrange
     experiment_summary %>%
@@ -33,51 +59,76 @@
             theme_light()
     
     
-## 3. Plotting proportion of annotations correct at diff AI thresholds
+## 4. Plotting proportion of annotations correct at diff AI thresholds
     
     # boxplot
     experiment_data %>%
-        ggplot(aes(confidence_threshold, propAIannotations_correct)) +
-            geom_boxplot(aes(fill = confidence_threshold), show.legend = F) +
-            scale_fill_viridis_d(alpha = 0.4) +
-            geom_signif(comparisons = list(c("50", "75")), 
-                        map_signif_level = TRUE,
-                        y_position = 1.01) +
-            labs(x = "AI Confidence Threshold",
-                 y = "AI Accuracy **(points correct/points labelled)**") +
-                theme_light()
+        filter(!confidence_threshold == "100") %>%
+        # mutate(propAIannotations_correct = replace_na(propAIannotations_correct, 1)) %>%
+            ggplot(aes(confidence_threshold, propAIannotations_correct)) +
+                geom_boxplot(aes(fill = confidence_threshold), show.legend = F) +
+                scale_fill_manual(values = alpha(c("#440154FF", "#238A8DFF"), 0.4)) +
+                # scale_fill_viridis_d(alpha = 0.4) +
+                geom_signif(test = "wilcox.test",
+                            comparisons = list(c("50", "75")),
+                            map_signif_level = TRUE) +
+                # geom_signif(test = "wilcox.test", 
+                #             comparisons = combn(
+                #                 levels(experiment_data$confidence_threshold),
+                #                 2,
+                #                 simplify = F),
+                #             step_increase = 0.2,
+                #             map_signif_level = T) +
+                labs(x = "Confidence Threshold",
+                     y = "Feature Extractor Accuracy") +
+                theme_pubr()
     
  
     # pointrange
     experiment_summary %>%
-        ggplot(aes(confidence_threshold, mean_AI_accuracy)) +
-        geom_pointrange(aes(ymin = mean_AI_accuracy - se_AI_accuracy,
-                            ymax = mean_AI_accuracy + se_AI_accuracy))+
-        theme_light()
+        filter(!confidence_threshold == "100") %>%
+            ggplot(aes(confidence_threshold, mean_AI_accuracy)) +
+                geom_pointrange(aes(ymin = mean_AI_accuracy - se_AI_accuracy,
+                                    ymax = mean_AI_accuracy + se_AI_accuracy))+
+                theme_light()
 
     
-## 4. Plotting number annotations unknown by AI vs time spent
+## 5. Plotting proportion annotations unknown by AI vs time spent
     
     experiment_data %>%
-        ggplot(aes(n_todo_annotations/50, time_spent)) +
+        ggplot(aes(propannotations_byAI, time_spent)) +
             geom_point(aes(color = confidence_threshold), 
                        alpha = 0.6) +
             scale_color_viridis_d() +
             geom_smooth(method = "lm") +
-            labs(x = "Proportion of Points Unconfirmed by AI",
-                 y = "Time Spent on Photo (sec)", 
+            # geom_smooth(method = "loess") +
+            labs(x = "Proportion of Points Confirmed by Feature Extractor",
+                 y = "Time Spent Annotating Photo (sec)", 
                  color = "Confidence Threshold") +
-            theme_light()
+            theme_pubr(legend = "bottom")
     
-    
-## 5. Proportion annotations connfirmed by AI vs prop annotations correct by AI
-    
-    experiment_data %>%
-        ggplot(aes(propAIannotations_confirmed, propAIannotations_correct)) +
-            geom_point(aes(color = confidence_threshold)) +
-            geom_smooth(method = "lm") +
-            scale_color_viridis_d() +
-            theme_light()
-    
+  
+## 6. Plotting potentially problematic artifacts    
+      
+    # proportion annotations confirmed by AI vs prop annotations correct by AI
         
+        experiment_data %>%
+            filter(!confidence_threshold == "100") %>%
+                ggplot(aes(propannotations_byAI, propAIannotations_correct)) +
+                    geom_point(aes(color = confidence_threshold)) +
+                    geom_smooth(method = "lm") +
+                    scale_color_viridis_d() +
+                    theme_light()
+        
+     
+    # time vs prop annotations correct by AI: not an artifact, unrelated     
+        
+        experiment_data %>%
+            filter(!confidence_threshold == "100") %>%
+            ggplot(aes(time_spent, propAIannotations_correct)) +
+                geom_point(aes(color = confidence_threshold)) +
+                geom_smooth(method = "lm") +
+                scale_color_viridis_d() +
+                theme_light()
+    
     
